e146af7fca5298f2e56ec50aae43657aeb484fb1
Jose angel <73683594+josearg150@users.noreply.github.com>
2022-12-14 21:19:26 -0600

    s

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,7 +4,12 @@ module.exports = defineConfig({
     'vuetify'
   ],
   devServer: {
+    proxy: {
+    '/api':{
+      target: "http://20.228.159.119:5000",
+    }
     //  proxy: "http://localhost:5000",
-       proxy: "http://20.228.159.119:5000",
+      //  proxy: "http://20.228.159.119:5000",
   }
+}
 })
