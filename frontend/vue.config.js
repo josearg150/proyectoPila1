7a9368efdd5544fdcbaba1e961c0f9d906649007
Jose angel <73683594+josearg150@users.noreply.github.com>
2022-12-14 21:25:49 -0600

    as

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,6 +7,8 @@ module.exports = defineConfig({
     proxy: {
     '/api':{
       target: "http://20.228.159.119:5000",
+      ws: true,
+      changeOrigin: true
     }
     //  proxy: "http://localhost:5000",
       //  proxy: "http://20.228.159.119:5000",
